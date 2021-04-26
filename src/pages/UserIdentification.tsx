0d501a33c96d1466213213c8a8a343d7f64df35b
Aline Alves <48930961+afsalves@users.noreply.github.com>
2021-04-26 07:28:13 -0300

    Adding navigation on App

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -6,11 +6,15 @@ import {
     Text,
     TextInput,
     KeyboardAvoidingView,
+    TouchableWithoutFeedback,
     Platform,
+    Keyboard,
 } from 'react-native';
 
+import { useNavigation } from "@react-navigation/core";
 
-import { Button } from '../components/Button'
+
+import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -19,6 +23,8 @@ export function UserIdentification(){
     const [isFocused, setIsfocused] = useState(false);
     const [isFilled, setIsFilled] = useState(false);
     const [name, setName] = useState<string>(); //estou forçando a tipagem dele como string
+
+    const navigation = useNavigation();
  
     function handleInputBlur(){
         setIsfocused(false);
@@ -31,45 +37,53 @@ export function UserIdentification(){
 
     function handleInputChange(value: string){
         setIsFilled(!!value);
-        setName(value)
+        setName(value);
+    }
+
+    function handleSubmmit(){
+        navigation.navigate('Confirmation');
     }
 
     return(
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView
                 style={styles.container}
-                behavior={Platform.OS === 'ios' ? 'padding': 'heigth'} //para não deixar o teclado esconder elementos na tela
+                behavior={Platform.OS === 'ios' ? 'padding' : 'heigth'} //para não deixar o teclado esconder elementos na tela
             >
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <View style={styles.header}> 
-                            <Text style={styles.emoji}>
-                               { isFilled ? '😄' : '😀' }
-                            </Text>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}> 
+                                <Text style={styles.emoji}>
+                                { isFilled ? '😄' : '😀' }
+                                </Text>
 
-                            <Text style={styles.title}>
-                            Como podemos {'\n'}
-                            chamar você?
-                            </Text>
-                        </View>
-                            <TextInput 
-                                style={[
-                                    styles.input,
-                                    (isFocused || isFilled) && 
-                                    { borderColor: colors.green } //alterando a cor quando o elemento está clicado
-                                ]}
-                                placeholder="Digite um nome"
-                                onBlur={handleInputBlur}
-                                onFocus={handleInputFocus}
-                                onChangeText={handleInputChange}
-                            />
-                         
-                        <View style={styles.footer}>
-                            <Button />
+                                <Text style={styles.title}>
+                                Como podemos {'\n'}
+                                chamar você?
+                                </Text>
+                            </View>
+                                <TextInput 
+                                    style={[
+                                        styles.input,
+                                        (isFocused || isFilled) && 
+                                        { borderColor: colors.green } //alterando a cor quando o elemento está clicado
+                                    ]}
+                                    placeholder='Digite um nome'
+                                    onBlur={handleInputBlur}
+                                    onFocus={handleInputFocus}
+                                    onChangeText={handleInputChange}
+                                />
+                            
+                            <View style={styles.footer}>
+                                <Button 
+                                    title ='Confirmar'
+                                    onPress={handleSubmmit}
+                                />
+                            </View>
                         </View>
                     </View>
-
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
